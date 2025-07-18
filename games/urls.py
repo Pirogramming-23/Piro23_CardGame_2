6651e6b21fa4a1359c21c4e96e8bc629930ea4d3
ZinYan <zinwaiyan2226@gmail.com>
2025-07-18 11:13:15 +0900

    [feat] game+user 머지

--- games/urls.py
+++ games/urls.py
@@ -5,9 +5,9 @@ app_name = "Game"
 
 urlpatterns = [
     path('<int:pk>',detail,name='detail'),
+    path('delete/<int:pk>',delete,name="delete"),
+    path('ranking',ranking,name="ranking"),
     path("create/", games_create),
     path("<int:pk>/cnt_attack", counter_attack),
     path("<int:pk>/result", games_result),
 ]
-
-
--- games/views.py
+++ games/views.py
@@ -23,6 +23,22 @@ def detail(request,pk):
     }
     return render(request,'games/detail.html',context)
 
+# 유저는 자신이 공격한 게임(상대가 반격하지 않은 경우에 한해) 삭제 가능
+def delete(request,pk):
+    game = Game.objects.get(id=pk)
+    if request.user == game.attacker and game.defender is None:
+        game.delete()
+    return redirect('games:main')
+
+
+def ranking(request):
+    users = User.objects.all().order_by('-user_score')[:3]
+    context ={
+        'users':users
+    }
+    return render(request,'games/ranking.html',context)
+
+
 def games_create(request):
     users = User.objects.all() #수정할 곳!
     numbers = random.sample(range(1, 11), 5)
