acef8288419b641fc332ecc4140f7cb510f9dd31
ParkJueun <jueun036@gmail.com>
2025-07-18 11:02:59 +0900

    user 연결 수정

--- games/urls.py
+++ games/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("create/", games_create),
-    path("<int:pk>/cnt_attack", counter_attack),
-    path("<int:pk>/result", games_result),
+    path("<int:upk>/create/", games_create),
+    path("<int:upk>/<int:gpk>/cnt_attack", counter_attack),
+    path("<int:upk>/<int:gpk>/result", games_result),
 ]
